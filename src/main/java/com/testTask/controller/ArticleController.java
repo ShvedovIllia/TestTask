0ac7d5ace8e879a5fb92b84c8dc0ae5771a213b0
ShvedovIllia <shvedov.illia@gmail.com>
2019-02-13 17:48:40 +0200

    add logs

--- src/main/java/com/testTask/controller/ArticleController.java
+++ src/main/java/com/testTask/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.testTask.entity.models.ArticleRequest;
 import com.testTask.services.impl.ArticleServiceImpl;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "article/")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -22,6 +24,7 @@ public class ArticleController {
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public ResponseEntity addArticle(@RequestBody final ArticleRequest articleRequest) throws NotFoundException {
+        log.info("add new article with color:" + articleRequest.getColor());
         articleService.saveArticle(articleConverter.convert(articleRequest));
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/testTask/controller/UserController.java
+++ src/main/java/com/testTask/controller/UserController.java
@@ -8,6 +8,7 @@ import com.testTask.enums.Colors;
 import com.testTask.services.UserService;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/users")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -25,25 +27,28 @@ public class UserController {
 
     @RequestMapping(value = "/{age}", method = RequestMethod.GET)
     public ResponseEntity<List<UserResponse>> getAllUsersWhenAgeBiggerThan(@PathVariable("age") Integer age) throws NotFoundException {
+        log.info("get all users, when age bigger, than" + age);
         List<UserEntity> userEntities = userService.getAllUsersWhereAgeBiggerThenN(age);
         return ResponseEntity.ok(userEntities.stream().map(userConverter::convert).collect(Collectors.toList()));
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ResponseEntity saveUser(@RequestBody UserRequest userRequest) {
+        log.info("create new user with " + userRequest.getName() + " and " + userRequest.getAge() + " age");
         userService.saveUser(userConverter.convert(userRequest));
         return ResponseEntity.ok().build();
     }
 
     @RequestMapping(value = "/byColor/{color}", method = RequestMethod.GET)
     public ResponseEntity<List<UserResponse>> getUsersByArticleColor(@PathVariable("color") Colors colors) {
+        log.info("get users by article color: " + colors);
         List<UserEntity> userEntities = userService.getUsersByArticleEntitiesColor(colors);
-
         return ResponseEntity.ok(userEntities.stream().map(userConverter::convert).collect(Collectors.toList()));
     }
 
     @RequestMapping(value = "/articleCount/", method = RequestMethod.GET)
     public ResponseEntity<List<String>> getUsersWhereArticlesGreaterThan() throws NotFoundException {
+        log.info("get name of users, whose have more than 3 articles");
         return ResponseEntity.ok(userService.getAllUsersWhereArticlesCountGreaterThan());
     }
 }
